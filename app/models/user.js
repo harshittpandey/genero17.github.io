2834c8338fcbd4e37308a7bcc0882507a91ac6f1
Abhishek Chopra <phoenix.prog1796@gmail.com>
2017-10-01 21:29:44 +0530

    housecleaning for bcgk ;)

--- app/models/user.js
+++ app/models/user.js
@@ -1,45 +1,44 @@
 //var sequelize = require('sequelize') ;
 var Sequelize = require('sequelize');;
-var sequelize = new Sequelize('loginapp','purnima','Genero20!7',{
-  port : 4000 ,
-  dialect : 'mysql'
-})  ;
-var bcrypt   = require('bcrypt-nodejs');
-var userSchema = sequelize.define('user',{
-
-    "local"            : {
-			  type : 'object'
-        "email"        : Sequelize.STRING ,
-        "password"     : Sequelize.STRING
-    },
-    "facebook"         : {
-				type : 'object'
-        "id"           : Sequelize.STRING ,
-        "token"        : Sequelize.STRING ,
-        "email"        : Sequelize.STRING ,
-        "name"         : Sequelize.STRING
-    },
-    "twitter"          : {
-				type : 'object'
-        "id"           : Sequelize.STRING ,
-        "token"        : Sequelize.STRING ,
-        "displayName"  : Sequelize.STRING ,
-        "username"     : Sequelize.STRING
-    },
-    "google"           : {
-				type : 'object'
-				"id"           : Sequelize.STRING ,
-        "token"        : Sequelize.STRING ,
-        "email"        : Sequelize.STRING ,
-        "name"         : Sequelize.STRING
-    }
+var sequelize = new Sequelize('loginapp', 'purnima', 'Genero20!7', {
+    port: 3306,
+    dialect: 'mysql'
+});
+var bcrypt = require('bcrypt-nodejs');
+var localUserSchema = sequelize.define('user', {
+    "email": Sequelize.STRING,
+    "password": Sequelize.STRING
 });
 
-userSchema.methods.generateHash = function(password) {
-   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// "local"            : {
+    //},
+    // "facebook"         : {
+    // 			type : 'object'
+    //     "id"           : Sequelize.STRING ,
+    //     "token"        : Sequelize.STRING ,
+    //     "email"        : Sequelize.STRING ,
+    //     "name"         : Sequelize.STRING
+    // },
+    // "twitter"          : {
+    // 			type : 'object'
+    //     "id"           : Sequelize.STRING ,
+    //     "token"        : Sequelize.STRING ,
+    //     "displayName"  : Sequelize.STRING ,
+    //     "username"     : Sequelize.STRING
+    // },
+    // "google"           : {
+    // 			type : 'object'
+    // 			"id"           : Sequelize.STRING ,
+    //     "token"        : Sequelize.STRING ,
+    //     "email"        : Sequelize.STRING ,
+    //     "name"         : Sequelize.STRING
+    // }
+
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-userSchema.methods.validPassword = function(password) {
-   return bcrypt.compareSync(password, this.local.password);
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
 };
